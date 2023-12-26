87c5bbb017b4b05a6bb6ee8eeb67ad95e2886e20
tiq0015 <tiq0015@naver.com>
2023-12-26 15:42:40 +0900

    2023.12.26 나이

--- src/com/sori/sol01/Eyyyy.java
+++ src/com/sori/sol01/Eyyyy.java
@@ -7,21 +7,37 @@ public class Eyyyy {
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
-		int mon;
+//		int mon;
+//		
+//		System.out.print("근무시간 : ");
+//		mon = in.nextInt();
+//		
+//		if(mon > 8)
+//		{
+//			System.out.println("임금은 " + (mon * 12000) + "원 입니다.");
+//		}
+//		if(mon <= 8)
+//		{
+//			System.out.println("임금은 " + (mon * 10000) + "원 입니다.");
+//		}
 		
-		System.out.print("근무시간 : ");
-		mon = in.nextInt();
+		int age;
 		
-		if(mon > 8)
-		{
-			System.out.println("임금은 " + (mon * 12000) + "원 입니다.");
+		System.out.print("나이를 입력하세요 : ");
+		age = in.nextInt();
+		
+		if(age >= 20) {
+			
+			System.out.println("성인입니다.");
 		}
-		if(mon <= 8)
-		{
-			System.out.println("임금은 " + (mon * 10000) + "원 입니다.");
-		}
 		
+		else {
+			System.out.println("미성년자입니다.");
+		}
 		in.close();
+		
+		
+		
 	}
 
 }
