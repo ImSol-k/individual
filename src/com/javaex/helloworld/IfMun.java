d99697b8ecdfba4ae4caef88208241ee1cbc864f
tiq0015 <tiq0015@naver.com>
2023-12-26 18:22:18 +0900

    2023.12.26 수업끝

--- src/com/javaex/helloworld/IfMun.java
+++ src/com/javaex/helloworld/IfMun.java
@@ -15,15 +15,16 @@ public class IfMun {
 		
 		System.out.print("근무시간 : ");
 		time = in.nextInt();
-		/*
+		
 		if(time <= 8) {
 			System.out.println("임금은 " + time * 10000 + "원 입니다.");
 		}
 		else {
-			System.out.println("임금은 " + time * 12000 + "원 입니다.");
+			System.out.println("임금은 " + (10000 * 8 + (time - 8) * 12000) + "원 입니다.");
 		}
-		*/
 		
+		
+		/*
 		//8시간까지는 시간당 10,000
 		//8시간 초과시 시간당 1.5배
 		int eight = time * 10000;
@@ -34,7 +35,7 @@ public class IfMun {
 		else {
 			System.out.println("임금은 " + (80000 + (time - 8) * 10000 / 2 * 3)  + "원 입니다.");
 		}
-		
+		*/
 		
 		
 		in.close();
