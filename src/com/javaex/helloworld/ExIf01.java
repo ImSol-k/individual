91dcf0bb404cf6ae9cf3526fbe21b42c63921ae8
tiq0015 <tiq0015@naver.com>
2023-12-27 10:54:01 +0900

    2023.12.27 변수이용

--- src/com/javaex/helloworld/ExIf01.java
+++ src/com/javaex/helloworld/ExIf01.java
@@ -5,28 +5,29 @@ import java.util.Scanner;
 public class ExIf01 {
 
 	public static void main(String[] args) {
-		
-		//알바 시급계산
-		//8시간까지는 시간당 10,000
-		//8시간초과는 시간당 12,000
-		
+
+		// 알바 시급계산
+		// 8시간까지는 시간당 10,000
+		// 8시간초과는 시간당 12,000
+
 		Scanner in = new Scanner(System.in);
 		int time;
-		
+		int pay;
+
 		System.out.print("근무시간 : ");
 		time = in.nextInt();
-		
-		if(time <= 8) {
-			System.out.println("임금은 " + time * 10000 + "원 입니다.");
+
+		if (time <= 8) {
+			pay = time * 10000;
+			//System.out.println("임금은 " + time * 10000 + "원 입니다.");
+		} else {
+			pay = 10000 * 8 + (time - 8) * 12000;
+			//System.out.println("임금은 " + (10000 * 8 + (time - 8) * 12000) + "원 입니다.");
 		}
-		else {
-			System.out.println("임금은 " + (10000 * 8 + (time - 8) * 12000) + "원 입니다.");
-		}
 		
-		
+		System.out.println("임금은 " + pay + "원 입니다.");
 		in.close();
-		
-		
+
 	}
 
 }
