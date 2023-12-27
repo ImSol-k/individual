0bbb5e71f45b67ae387831511df27fca6dbabf8a
tiq0015 <tiq0015@naver.com>
2023-12-27 15:12:07 +0900

    switch 묶어서 사용해보기

--- src/com/javaex/helloworld/ExSwitch.java
+++ src/com/javaex/helloworld/ExSwitch.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class ExSwitch {
 
 	public static void main(String[] args) {
-		
+
+		// 해당월의 일수 출력
+
 		Scanner in = new Scanner(System.in);
 		int mon;
-		
+
 		System.out.println("월을 입력하세요");
 		mon = in.nextInt();
-		
+/*
 		switch (mon) {
 		case 1:
 			System.out.println("31일 입니다.");
@@ -49,8 +51,31 @@ public class ExSwitch {
 		case 12:
 			System.out.println("31일 입니다.");
 			break;
+		default:
+			System.out.println("잘못입력");
+			break;
 		}
-		
+*/
+		switch (mon) {
+		case 1 :
+		case 3 :
+		case 5 :
+		case 7 :
+		case 8 :
+		case 10 :
+		case 12 :
+			System.out.println("31일 입니다.");
+			break;
+		case 4 :
+		case 6 :
+		case 9 :
+		case 11 :
+			System.out.println("30일 입니다.");
+			break;
+		case 2 :
+			System.out.println("28일 입니다.");
+			break;
+		}
 		in.close();
 	}
 
