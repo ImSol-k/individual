585a8ef90b3381fe2a2c54ad05438d8d71e119cb
tiq0015 <tiq0015@naver.com>
2023-12-30 15:29:19 +0900

    2023.12.30 대박 반대로 나오지만 정렬성공

---
+++ src/com/sori/sol01/Rotto.java
@@ -0,0 +1,50 @@
+package com.sori.sol01;
+
+import java.util.Iterator;
+
+public class Rotto {
+
+	public static void main(String[] args) {
+		
+		int[] rotto = new int[6];
+		int temp;
+		
+		for (int i = 0; i < rotto.length; i++) {
+			rotto[i] = (int)(Math.random() * 45) + 1;
+			for (int j = 0; j < i; j++) {
+				if(rotto[i] == rotto[j]) {
+					i--;
+				}
+			}
+		}
+		
+		System.out.println("==================");
+		
+		for (int i = 0; i < rotto.length; i++) {
+			System.out.print(rotto[i] + " ");
+		}
+		
+		System.out.println();
+		System.out.println("==================");
+		
+		
+		for (int i = 0; i < rotto.length; i++) {
+			
+			for (int j = 0; j < i; j++) {
+				
+				if(rotto[i] > rotto[j]) {
+					temp = rotto[i];
+					rotto[i] = rotto[j];
+					rotto[j] = temp;
+				}
+			}
+		}
+		
+		for (int i = 0; i < rotto.length; i++) {
+			System.out.print(rotto[i] + " ");
+		}
+		
+		
+	}
+
+}
