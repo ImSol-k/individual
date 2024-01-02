91cb95e7c03a15de5cad1358d833522481a24b41
tiq0015 <tiq0015@naver.com>
2024-01-02 18:11:07 +0900

    연습

---
+++ src/com/javaex/practice01/Ex29.java
@@ -0,0 +1,18 @@
+package com.javaex.practice01;
+
+import java.util.Scanner;
+public class Ex29 {
+
+	public static void main(String[] args) {
+		
+		//직사각형의 둘레와 면적 구하기
+		//가로세로 변수 double형으로 작성
+		
+		Scanner in = new Scanner(System.in);
+		int h, w;
+		
+		System.out.println("가로를 입력하세요");
+		
+	}
+
+}
